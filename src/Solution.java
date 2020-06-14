a5b20e79a0e291af099f228437961370a4c471c6
ItIsMe7 <66698441+ItIsMe7@users.noreply.github.com>
2020-06-14 18:05:36 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Person p1 = new Person(false, "Tania");
+        Person p2 = new Person(false, "Anna");
+        Person p3 = new Person(true, "Jon");
+        Person p4 = new Person(true, "Lev");
+        Person p5 = new Person(true, "Ivan");
+    }}
